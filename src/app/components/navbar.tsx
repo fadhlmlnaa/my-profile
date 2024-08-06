a40809f427cc4fedf8aed8ec7111f7be9ffee894
Fadhil Maulana Syhafiq <fadhlmlnaa@gmail.com>
2024-08-06 10:47:23 +0700

    style: change navbar bg color

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -21,7 +21,7 @@ export default function Example() {
   return (
     <>
       <header>
-        <Disclosure as="nav" className="dark:bg-gray-800 bg-white">
+        <Disclosure as="nav" className="dark:bg-black bg-white">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
